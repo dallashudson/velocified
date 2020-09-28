4412a07b471dccf4eec85a796b58a3d88c0bb997
dallashudson <dallashyz@gmail.com>
2020-09-28 17:20:51 -0500

    updated desc in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,11 +18,11 @@ const config = {
   ** Headers of the page
   */
   head: {
-    title: 'Custom app with Contentful',
+    title: 'Velocified',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Custom app in under 5 minutes' }
+      { hid: 'description', name: 'description', content: 'Serving the sickest original content' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
